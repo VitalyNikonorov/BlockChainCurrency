e594f2ff8042c2c9d4e16eb547c42b662283de0d
VitalyNikonorov <nik.vitaly@mail.ru>
2018-03-10 19:28:57 +0300

    optimized imports

--- app/src/main/java/net/nikonorov/blockchaincurrency/di/AppModule.kt
+++ app/src/main/java/net/nikonorov/blockchaincurrency/di/AppModule.kt
@@ -3,19 +3,9 @@ package net.nikonorov.blockchaincurrency.di
 import android.content.Context
 import dagger.Module
 import dagger.Provides
-import net.nikonorov.blockchaincurrency.App
-import net.nikonorov.blockchaincurrency.BuildConfig
 import net.nikonorov.blockchaincurrency.data.NetworkClient
 import net.nikonorov.blockchaincurrency.domain.CurrencyInteractor
 import net.nikonorov.blockchaincurrency.domain.CurrencyInteractorImpl
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import ru.terrakok.cicerone.Cicerone
-import ru.terrakok.cicerone.NavigatorHolder
-import ru.terrakok.cicerone.Router
 import javax.inject.Singleton
 
 
