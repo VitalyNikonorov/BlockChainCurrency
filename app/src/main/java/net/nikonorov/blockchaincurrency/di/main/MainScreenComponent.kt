4919facb561c6689d6b9e81174ffd14eaa7ac6f9
VitalyNikonorov <nik.vitaly@mail.ru>
2018-03-11 02:30:14 +0300

    syntax changing

--- app/src/main/java/net/nikonorov/blockchaincurrency/di/main/MainScreenComponent.kt
+++ app/src/main/java/net/nikonorov/blockchaincurrency/di/main/MainScreenComponent.kt
@@ -7,7 +7,7 @@ import net.nikonorov.blockchaincurrency.presentation.main.view.MainFragment
  * Created by Vitaly Nikonorov on 08.03.2018.
  * email@nikonorov.net
  */
-@Subcomponent(modules = arrayOf(MainScreenModule::class))
+@Subcomponent(modules = [(MainScreenModule::class)])
 @MainScreenScope
 interface MainScreenComponent {
     fun inject(mainFragment: MainFragment)
--- app/src/main/java/net/nikonorov/blockchaincurrency/di/pairinfo/PairInfoScreenComponent.kt
+++ app/src/main/java/net/nikonorov/blockchaincurrency/di/pairinfo/PairInfoScreenComponent.kt
@@ -7,7 +7,7 @@ import net.nikonorov.blockchaincurrency.presentation.info.view.PairInfoFragment
  * Created by Vitaly Nikonorov on 10.03.2018.
  * email@nikonorov.net
  */
-@Subcomponent(modules = arrayOf(PairInfoModule::class))
+@Subcomponent(modules = [(PairInfoModule::class)])
 @PairInfoScreenScope
 interface PairInfoScreenComponent {
     fun inject(pairInfoFragment: PairInfoFragment)
